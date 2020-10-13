37ba94c970650668374cc37c688cc217f33e9da9
Nishant Meher <nishant.meher@roundpointmortgage.com>
2020-10-14 00:19:51 +0530

    commit the code

---
+++ performancemessagesender/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace performancemessagesender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://salesteamapp-nm1234.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mBOysToPADu1cgll6yOH+yyefeEOycmSec2kU08HX8g=";
+        const string TopicName = "salesperformancemessages";
+        static ITopicClient topicClient;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sending a message to the Sales Performance topic...");
+
+            SendPerformanceMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+
+        }
+
+        static async Task SendPerformanceMessageAsync()
+        {
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            // Send messages.
+            try
+            {
+                string messageBody = $"Total sales for Brazil in August: $13m.";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                // Write the body of the message to the console.
+                Console.WriteLine($"Sending message: {messageBody}");
+
+                // Send the message to the queue.
+                await topicClient.SendAsync(message);
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            await topicClient.CloseAsync();
+        }
+    }
+}
---
+++ privatemessagesender/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace privatemessagesender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://salesteamapp-nm1234.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mBOysToPADu1cgll6yOH+yyefeEOycmSec2kU08HX8g=";
+        const string QueueName = "salesmessages";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending a message to the Sales Messages queue...");
+
+            SendSalesMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+        }
+
+        static async Task SendSalesMessageAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            // Send messages.
+            try
+            {
+                string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                // Write the body of the message to the console.
+                Console.WriteLine($"Sending message: {messageBody}");
+
+                // Send the message to the queue.
+                await queueClient.SendAsync(message);
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            await queueClient.CloseAsync();
+        }
+    }
+}
